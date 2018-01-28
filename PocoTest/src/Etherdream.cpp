d398b8709cab3c4c920eb5f761ca29b2e3103954
Seb Lee-Delisle <seb@leedelisle.com>
2018-01-28 12:02:46 +0000

    Deleting unused apps

---
+++ PocoTest/src/Etherdream.cpp
@@ -0,0 +1,9 @@
+//
+//  etherdream.cpp
+//  PocoTest
+//
+//  Created by Seb Lee-Delisle on 02/11/2017.
+//
+//
+
+#include "etherdream.hpp"
---
+++ PocoTest/src/Etherdream.h
@@ -0,0 +1,20 @@
+//
+//  etherdream.hpp
+//  PocoTest
+//
+//  Created by Seb Lee-Delisle on 02/11/2017.
+//
+//
+#include "ofMain.h"
+#pragma once
+
+class Etherdream : public ofThread{
+	
+	public :
+	
+	
+
+
+
+
+}
