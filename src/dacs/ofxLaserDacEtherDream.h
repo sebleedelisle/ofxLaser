4fb1ca82650b935f3bab3ba6cf3aa504ab8c18e6
Seb Lee-Delisle <seb@leedelisle.com>
2022-03-09 04:29:25 -0800

    adding usleep for windows

--- src/dacs/ofxLaserDacEtherDream.h
+++ src/dacs/ofxLaserDacEtherDream.h
@@ -90,7 +90,20 @@ public:
     
     
 private:
-    
+#ifdef _MSC_VER
+    void usleep(unsigned int usec)
+    {
+        HANDLE timer;
+        LARGE_INTEGER ft;
+
+        ft.QuadPart = -(10 * (__int64)usec);
+
+        timer = CreateWaitableTimer(NULL, TRUE, NULL);
+        SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+        WaitForSingleObject(timer, INFINITE);
+        CloseHandle(timer);
+    }
+#endif
     void threadedFunction() override;
   
     inline bool sendBegin();
