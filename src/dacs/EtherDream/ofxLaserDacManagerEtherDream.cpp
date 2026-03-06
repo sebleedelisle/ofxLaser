5d0daec434568d20f316b7314865403777f5e43e
Seb Lee-Delisle <seb@leedelisle.com>
2026-03-06 11:39:44 +0000

    Revert ether dream discovery!

--- src/dacs/EtherDream/ofxLaserDacManagerEtherDream.cpp
+++ src/dacs/EtherDream/ofxLaserDacManagerEtherDream.cpp
@@ -7,6 +7,7 @@
 
 #include "ofxLaserDacManagerEtherDream.h"
 #include "ByteStreamUtils.h"
+#include "ofxLaserDacEtherDreamResponse.h"
 
 using namespace ofxLaser;
 
@@ -14,11 +15,18 @@ DacManagerEtherDream :: DacManagerEtherDream()  {
     ofxUDPSettings settings;
     settings.bindPort = 7654;
     settings.blocking = false;
+    settings.reuse = true;
+    verbose = false;
     if(!udpConnection.Setup(settings)) {
-        ofLogError("DacManagerEtherDream : udp set up failed");
+        ofLogError("DacManagerEtherDream")
+            << "UDP bind failed on *:7654 (Ether Dream discovery). "
+            << "Will keep retrying in the background. Another app may already own this port "
+            << "(for example MadMapper). Check with: lsof -nP -iUDP:7654";
+    } else {
+        ofLogNotice("DacManagerEtherDream")
+            << "Ether Dream discovery listening on UDP :7654 (reuse enabled)";
     }
     startThread();
-    verbose = false;
 }
 
 DacManagerEtherDream :: ~DacManagerEtherDream()  {
@@ -51,11 +59,44 @@ void DacManagerEtherDream :: threadedFunction() {
     SetThreadPriority( thread.native_handle(), THREAD_PRIORITY_LOWEST);
 #endif
     // sleep a bit the first time! Make sure not to overwhelm everything
-    while(ofGetElapsedTimeMillis() < startUpDelayMS) {
-        sleep(50);
-    }
+    sleep(startUpDelayMS);
     
+    const uint64_t rebindRetryIntervalMs = 2000;
+    const uint64_t rebindLogIntervalMs = 10000;
+    uint64_t nextRebindAttemptMs = 0;
+    uint64_t lastRebindLogMs = 0;
+    bool receiveErrorLogged = false;
+
+    auto tryBindDiscoverySocket = [&]() -> bool {
+        if(udpConnection.HasSocket()) {
+            return true;
+        }
+        ofxUDPSettings settings;
+        settings.bindPort = 7654;
+        settings.blocking = false;
+        settings.reuse = true;
+        if(udpConnection.Setup(settings)) {
+            ofLogNotice("DacManagerEtherDream")
+                << "Recovered Ether Dream discovery socket on UDP :7654";
+            return true;
+        }
+        return false;
+    };
+
     while(isThreadRunning()) {
+        if(!udpConnection.HasSocket()) {
+            uint64_t nowMs = ofGetElapsedTimeMillis();
+            if(nowMs >= nextRebindAttemptMs) {
+                if(!tryBindDiscoverySocket() && ((lastRebindLogMs == 0) || ((nowMs - lastRebindLogMs) >= rebindLogIntervalMs))) {
+                    ofLogError("DacManagerEtherDream")
+                        << "Still unable to bind UDP :7654 for Ether Dream discovery. Retrying...";
+                    lastRebindLogMs = nowMs;
+                }
+                nextRebindAttemptMs = nowMs + rebindRetryIntervalMs;
+            }
+            sleep(100);
+            continue;
+        }
         
         // LET'S ASSUME FOR NOW...
         // that every packet is a complete message from a single dac.
@@ -64,11 +105,22 @@ void DacManagerEtherDream :: threadedFunction() {
         do {
             memset(udpMessage,0,sizeof(udpMessage));
             numBytesReceived = udpConnection.Receive(udpMessage,packetSize); //returns number of bytes received
+            if(numBytesReceived == SOCKET_ERROR) {
+                if(!receiveErrorLogged) {
+                    ofLogError("DacManagerEtherDream")
+                        << "UDP receive failed on discovery socket (:7654). "
+                        << "Closing socket and retrying bind.";
+                    receiveErrorLogged = true;
+                }
+                udpConnection.Close();
+                nextRebindAttemptMs = 0;
+            } else {
+                receiveErrorLogged = false;
+            }
             
             // ofLogNotice("Received "+ ofToString(numBytesReceived) + " bytes from UDP connection");
             if(numBytesReceived >=36)  {
-                
-                double now = static_cast<double>(ofGetElapsedTimeMillis()) / 1000.0;
+                const double now = ofGetElapsedTimef();
                 
                 string address;
                 int port;
@@ -103,8 +155,8 @@ void DacManagerEtherDream :: threadedFunction() {
                 byteaddress+=4;
                 
                 
-                //unsigned char* buffer = byteaddress-2;
-                // Legacy status parsing removed; we only care about discovery here.
+                DacEtherDreamStatus status;
+                status.deserialize(byteaddress);
                 
 //                            cout << "Hardware version :" << hardwareRevision << endl;
 //                            cout << "Software version :" << softwareRevision << endl;
@@ -122,35 +174,28 @@ void DacManagerEtherDream :: threadedFunction() {
                 //sprintf(idchar, "%llX", macAddress);
                 string id(idchar);
                 
-                // if we haven't already got this etherdream, then add it
-                if(etherdreamDataByMacAddress.find(id) == etherdreamDataByMacAddress.end()) {
-                    
-                    // ********************* REMOVE IN-USE CHECK FOR DUSTIN
-                    EtherDreamData ed;
-                    const bool inUse = false;
-                    ed = {hardwareRevision, softwareRevision, bufferCapacity, (int) maxPointRate, id, address, inUse, ofGetElapsedTimef()};
-                    
-                    if(verbose) {
-                        ofLogNotice("Adding etherdreamData "+ id)<< " " << hardwareRevision << " " << softwareRevision << " " << id << " in use : " << ed.inUse;
-                    }
-                    //ofLogNotice(status.toString());
-                    if(lock()) {
+                const bool inUse = checkInUse ? (status.playback_state != 0) : false;
+                if(lock()) {
+                    auto it = etherdreamDataByMacAddress.find(id);
+                    if(it == etherdreamDataByMacAddress.end()) {
+                        EtherDreamData ed = {hardwareRevision, softwareRevision, bufferCapacity, static_cast<int>(maxPointRate), id, address, inUse, now};
                         etherdreamDataByMacAddress[id] = ed;
                         dacsChanged = true;
-                        unlock();
-                    }
-                } else {
-                    
-                    if(lock()) {
-                        EtherDreamData& ed = etherdreamDataByMacAddress[id];
-                        
-                        ed.lastUpdateTime = now;
-                        // no live playback status tracking with libera-only path
                         if(verbose) {
-                            ofLogNotice("Updating etherdreamData "+ id)<< " " << hardwareRevision << " " << softwareRevision << " " << id << " in use : " << ed.inUse;
+                            ofLogNotice("Adding etherdreamData "+ id) << " " << hardwareRevision << " " << softwareRevision << " " << id << " in use : " << ed.inUse;
                         }
-                        unlock();
+                    } else {
+                        EtherDreamData& ed = it->second;
+                        ed.lastUpdateTime = now;
+                        if(ed.inUse != inUse) {
+                            ed.inUse = inUse;
+                            dacsChanged = true;
+                        }
+                        if(verbose) {
+                            ofLogNotice("Updating etherdreamData "+ id) << " " << hardwareRevision << " " << softwareRevision << " " << id << " in use : " << ed.inUse;
+                        }
                     }
+                    unlock();
                 }
                 
             }
@@ -171,7 +216,7 @@ void DacManagerEtherDream :: threadedFunction() {
                 }
             }
 
-            lastCheckTime = static_cast<double>(ofGetElapsedTimeMillis()) / 1000.0;
+            lastCheckTime = ofGetElapsedTimef();
             unlock();
         }
         for(int i = 0; (i<20) && isThreadRunning(); i++) {
@@ -208,17 +253,26 @@ std::shared_ptr<DacBase> DacManagerEtherDream :: getAndConnectToDac(const string
         if(verbose)  ofLogNotice("DacManagerEtherDream :: getAndConnectToDac(...) - Already a dac made with id "+ofToString(id));
         return dac;
     }
-    // todo check it exists!
-    if(etherdreamDataByMacAddress.find(id)!=etherdreamDataByMacAddress.end()) {
-        EtherDreamData& ed = etherdreamDataByMacAddress.at(id);
-        // MAKE DAC
-        DacEtherDream* edac = new DacEtherDream();
-        edac->setup(id, ed.ipAddress, ed);
-        dacsById.emplace(std::make_pair(id, edac));
-        return dacsById[id];
-    } else {
+    EtherDreamData ed;
+    bool found = false;
+    if(lock()) {
+        auto it = etherdreamDataByMacAddress.find(id);
+        if(it != etherdreamDataByMacAddress.end()) {
+            ed = it->second;
+            found = true;
+        }
+        unlock();
+    }
+
+    if(!found) {
         return nullptr;
     }
+
+    // MAKE DAC
+    auto edac = std::make_shared<DacEtherDream>();
+    edac->setup(id, ed.ipAddress, ed);
+    dacsById[id] = edac;
+    return edac;
 }
 
 
